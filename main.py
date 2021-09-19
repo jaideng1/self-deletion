74b7ac42757e3c8313843bbe3629c852eedb74b8
Jaiden <25446053+jaideng1@users.noreply.github.com>
2021-09-19 00:21:08 -0700

    idk why it didn't work before but now it does

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-inp = input("Do you want to delete this program?")
+inp = str(raw_input("Do you want to delete this program? "))
 
 if inp.lower() == "yes" or inp.lower() == "y":
     if os.path.exists("main.py"):
