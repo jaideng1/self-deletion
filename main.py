8bd43c0d8ba810b468e59deb812dd3399aa9eb19
Jaiden <25446053+jaideng1@users.noreply.github.com>
2021-09-19 00:17:24 -0700

    First Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+inp = input("Do you want to delete this program?")
+
+if inp.lower() == "yes" or inp.lower() == "y":
+    if os.path.exists("main.py"):
+        print("cya <o/")
+        os.remove("main.py")
+    else:
+        print("doesn't exist.")
+else:
+    print("oh. ok. guess i live another day.")
+
